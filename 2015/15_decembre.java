c52bd3c55075e954ca0d60a10f3a4bab7015c1dd
Boue Melaine <melaine.boue@etudiant.univ-rennes1.fr>
2018-12-01 08:16:42 +0100

    Debut exo 2

--- 2015/15_decembre.java
+++ 2015/15_decembre.java
@@ -2,9 +2,15 @@ Examen Prog 1 15 décembre 2015
 
 Exercice 1 
 
-/**
-
 public boolean existingCommune(String deptName; String communeName){
-	
-	
+	if(this.contains(deptName)){   
+		Set<String> communes = this.get(deptName);  
+		return communes != null && communes.contains(communeName);   
+	}
+	return false;	
 }
+
+
+Question 2
+
+
