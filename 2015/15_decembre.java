495a0444906ace5bbf53ba6a36ef8588810826fd
Boue Melaine <melaine.boue@etudiant.univ-rennes1.fr>
2018-12-01 09:34:13 +0100

    Debut exo 2 question 5

--- 2015/15_decembre.java
+++ 2015/15_decembre.java
@@ -12,5 +12,27 @@ public boolean existingCommune(String deptName; String communeName){
 
 
 Question 2
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+Exercice 2
 
+Question 4
 
+public int numberOfNodes()
+{
+	int number_of_nodes = 0;
+	Iterator<PostCard> it = iterator();
+	
+	while(it.nodeType()==SIMPLE_LEFT || t.nodeType()==DOUBLE)
+	{
+		number_of_nodes += it.getValue().rightTotal + 1; //le nombre du fils droit plus l'element courant
+		it.goLeft();
+	}	
+	return number_of_nodes;
+}
