bfca8fe42d520454457530c34cf0f84e934f3c66
Boue Melaine <melaine.boue@etudiant.univ-rennes1.fr>
2018-12-15 14:42:37 +0100

    Exercice 1 examen decembre 2014 terminé

--- 2014/14_decembre_2014.java
+++ 2014/14_decembre_2014.java
@@ -25,3 +25,63 @@ public Set<String> whichDays(String driver, String bus)
 	}
 	return jours;
 }
+
+Question 3
+public void printInfoForEachBus(Set<String> busSet)
+{
+	Iterator<String> it = iterator();
+	while(it.hasNext())
+	{
+		String driver = it.next();
+		DriverSchedule jours_conduite = getValue(driver);
+		Iterator<String> it2 = jours_conduite.iterator();
+		while(it2.hasNext())
+		{
+			String jour = it2.next();
+			String bus = jours_conduite.getValue(jour);
+			if(busSet.contains(bus))
+			{
+				System.out.println("("+driver+","+jour+","+bus+")");
+			}
+		}
+	}
+}
+
+
+
+Question 4 
+
+public void mergeSchedules(BusDepot newDepot)
+{
+	Iterator<String> it = newDepot.iterator();
+	while(it.hasNext())
+	{
+		String nouveau_driver = it.next();
+		DriverSchedule nouveau_programme = newDepot.getValue(nouveau_driver);
+		
+		if(this.contains(nouveau_driver))
+		{
+			DriverSchedule ancien_programme = this.getValue(nouveau_driver);
+			
+			Iterator<String> it2 = nouveau_programme.iterator();
+			while(it2.hasNext())
+			{
+				String jour = it2.next();
+				String bus = nouveau_programme.getValue(jour);
+				
+				if(ancien_programme.contains(jour))
+				{
+					ancien_programme.modifyValue(jour,bus);
+				}
+				else 
+				{
+					ancien_programme.addValue(jour,bus);
+				}
+			}
+		}
+		else
+		{
+			this.addValue(nouveau_driver,nouveau_programme);
+		}
+	}
+}
