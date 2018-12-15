5b642ebc89b666c38be19aff393b36a1066bd0e2
Boue Melaine <melaine.boue@etudiant.univ-rennes1.fr>
2018-12-15 06:11:29 +0100

    table

---
+++ 2015/15_juin_2015.java
@@ -0,0 +1,14 @@
+Exercice 24 Juin 2015
+
+Question 1
+
+public boolean checkCertificate(String employeeId, String certificate)
+{
+	Set<String> certificate_list = contains(employeeId);
+	return certificate!=null && certificate.contains(certificate);
+}
+
+
+Question 2
+
+public
