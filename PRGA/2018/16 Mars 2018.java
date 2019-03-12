acbf177db2cde4566986fa27c96928fe8be25739
Boue Melaine <melaine.boue@etudiant.univ-rennes1.fr>
2019-03-12 23:07:26 +0100

    PRGA1

--- PRGA/2018/16 Mars 2018.java
+++ PRGA/2018/16 Mars 2018.java
@@ -1,3 +1,5 @@
+Problème 1
+
 Question 1.1
 
 /* 
@@ -24,3 +26,55 @@ IterateurIntervalle iter = new IterateurIntervalle(1,5);
 	assertFalse(iter.hasNext()); //on a lu tous les nombres. hastNext() doit nous retourner false 
 }
 }
+
+
+Question 1.2
+/*
+Ce scénario ne couvre pas tous les cas.
+Il doit gérer le cas où l'exception est lévé par l'appel de next() après que hasNext() aie retourné false.
+*/
+
+
+
+Problème 2
+
+class TableNomen implements Nomenclature
+{
+	//on remarque que presque toutes prennent en paramètres le code, et retourne l'entree ou  le libelle
+	//donc on va stocker toutes les entrées dans une table (HashMap), le code(String) sera la clé et l'entrée (Entree) sa valeur
+	HashMap<Striing,Entree> les_entrees; //il stocke les entrées
+	
+	public TableNomen()
+	{
+		les_entrees = new HashMap<Striing,Entree>();
+	}
+	
+	
+	public Entree getEntree(String code)
+	{
+		return les_entrees.get(code); //retourne l'entree qui est associée à la clé code
+	}
+	
+	public String getLibelle(String code)
+	{
+		return les_entrees.get(code).getLibelle();
+	}
+	
+	public Entree getPere(Entree entree)
+	{
+		String codePere = les_entrees.get(entree.getCode()).getCodePere();
+		return les_entrees.get(codePere);
+	}
+	
+	public int getNiveau(Entree entree)
+	{
+		
+	}
+	public boolean ajoutPossible(Entree e);
+	public void ajouter(Entree e);
+}
+
+ajouterPossible()
+
+ajouter()
+
